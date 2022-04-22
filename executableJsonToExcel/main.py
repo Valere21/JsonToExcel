4e66b7edad393d131fbb8699f05fee2fba5fbb8f
gay-heuzey@et.esiea.fr <valere.gh@hotmail.fr>
2022-04-22 14:22:19 +0200

    Ajout script python finale

--- executableJsonToExcel/main.py
+++ executableJsonToExcel/main.py
@@ -1,19 +1,16 @@
 import json
 import pandas as pd
 import sys
-from os import listdir
-from os.path import isfile, join
-from datetime import datetime
 import os
 import glob
 import ctypes  # An included library with Python install.
-from tkinter import messagebox
+# import psutil
 
-# messagebox.showinfo("Basic Example", "a Basic Tk MessageBox")
+from os import listdir
+from os.path import isfile, join
+from datetime import datetime
 
 onlyfiles = [f for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))]
-# onlyfiles = [f for f in listdir("C:/JSONbrut/ModifiedJSON") if isfile(join("C:/JSONbrut/ModifiedJSON", f))]
-# print(onlyfiles)
 
 jsonList = []
 
@@ -27,18 +24,11 @@ dfRes = pd.json_normalize(jsonList)
 now = datetime.now()
 dt_string = 'Excel_data_JSON_railnova_' + now.strftime("%d-%m-%Y_%H-%M-%S")
 
-# p = psutil.Process(sys.argv[3])
-# p.terminate()  #or p.kill()
-
 dfRes.to_excel(sys.argv[2] + dt_string + '.xlsx')
 
 files = glob.glob(sys.argv[1] + "/*")
 for f in files:
     os.remove(f)
 
-# p = psutil.Process(sys.argv[3])
-# p.kill()  #or p.kill()
-
-# os.kill(sys.argv[3], signal.SIGTERM)  # or signal.SIGKILL
-
 ctypes.windll.user32.MessageBoxW(0, "Les données JSON ont fini d'être traitée", "Traitement réussie")
+sys.exit(0)
